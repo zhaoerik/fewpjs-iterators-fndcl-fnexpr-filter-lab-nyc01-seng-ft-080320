86259b344d3a38a2a98ed941dbc4b4a510acf0b7
Erik Zhao <erikzhao98@gmail.com>
2021-01-29 16:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution here
 let findMatching = function(arr, driver) {
     arr.filter(el => {
-        if (el.toLowerCase() === driver.toLowerCase()) return el
+        if (el.toLowerCase() === driver.toLowerCase()) {
+            return el
+        }
     })
 }
