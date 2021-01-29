03394c5b9347e71cd6d72b32cc43f51276f9af2f
Erik Zhao <erikzhao98@gmail.com>
2021-01-29 17:24:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+let findMatching = function (arr, driver) {
+    return arr.filter(el =>
+        el.toLowerCase() === driver.toLowerCase()
+    )
+}
+
+let fuzzyMatch = function (arr, driver) {
+    return arr.filter(el => {
+        if (el.indexOf(driver) === 0) return el
+    })
+}
+
+let matchName = function (arr, driver) {
+    return arr.filter(el =>
+        el.name === driver
+    )
+}
